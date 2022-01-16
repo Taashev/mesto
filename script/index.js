53ac322e392ddd326e7d502bc046012d784df235
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-01-16 15:38:49 +0300

    Добавть собитие при нажатии кнопки сохранить форма закрывается
    * Добавить фичу если при ред.проф закрыть окно через крестик то при следующем открытии
    формы значения инпутов обновляются и снова актуальные со страницы

--- script/index.js
+++ script/index.js
@@ -1,43 +1,37 @@
 let body = document.querySelector('body');
 let page = document.querySelector('.page');
-
-
-// popup Open end Close
 let buttonEdit = page.querySelector('.profile__edit-btn')
+let popupButtonClose = page.querySelector('.popup__close');
+let popupButtonSubmit = page.querySelector('.popup__submit')
 let popupProfile = page.querySelector('.popup_type_profile');
-
-function popupOpen(btn, popup) {
-  btn.addEventListener('click', function() {
-    body.classList.add('body_no-scroll');
-    popup.classList.toggle('popup_opened');
-  });
-};
-
-popupOpen(buttonEdit, popupProfile);
-
-function popupClose(popup) {
-  let popupButtonClose = page.querySelector('.popup__close');
-
-  popupButtonClose.addEventListener('click', function() {
-    body.classList.remove('body_no-scroll');
-    popup.classList.remove('popup_opened');
-  });
-};
-
-popupClose(popupProfile);
-
-
-// save profile changes
-// Находим форму в DOM
-let formElement = page.querySelector('.popup__container');
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__user-name');
-let jobInput = formElement.querySelector('.popup__about-me');
-
 let profileUserName = page.querySelector('.profile__user-name');
 let profileUserAbout = page.querySelector('.profile__user-about');
-nameInput.value = profileUserName.textContent;
-jobInput.value = profileUserAbout.textContent;
+
+
+// popup profile open end close
+  buttonEdit.addEventListener('click', function() {
+    body.classList.add('body_no-scroll');
+    popupProfile.classList.toggle('popup_opened');
+
+    nameInput.value = profileUserName.textContent;
+    jobInput.value = profileUserAbout.textContent;
+  });
+
+  function popupClose(button) {
+    button.addEventListener('click', function() {
+      body.classList.remove('body_no-scroll');
+      popupProfile.classList.remove('popup_opened');
+    });
+  };
+
+  popupClose(popupButtonClose);
+  popupClose(popupButtonSubmit);
+
+
+// save edit form profile
+let formElement = page.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__user-name');
+let jobInput = formElement.querySelector('.popup__about-me');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -51,8 +45,6 @@ function formSubmitHandler(evt) {
   profileUserName.textContent = nameInputValue;
   profileUserAbout.textContent = jobInputValue;
 }
-
-
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
