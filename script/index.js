ee6216e4ecb82d5f559946c2abda56ed15e42445
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-01-18 07:02:16 +0300

    Изменить структуру кода js
    * Добавить новую анимацию на popup__close
    * Изменить имена классов

--- script/index.js
+++ script/index.js
@@ -6,49 +6,60 @@ let profileUserName = page.querySelector('.profile__user-name');
 let profileUserAbout = page.querySelector('.profile__user-about');
 // let popup
 let popup = page.querySelector('.popup');
-let popupForm = popup.querySelector('.popup__container');
+let popupForm = popup.querySelector('.popup__form');
 let popupButtonClose = popupForm.querySelector('.popup__close');
-let popupButtonSubmit = popupForm.querySelector('.popup__submit')
+let popupButtonSave = popupForm.querySelector('.popup__button_type_save')
 // let popup edit profile
 let popupProfile = page.querySelector('.popup_type_profile');
-let popupNameInput = popupProfile.querySelector('.popup__user-name');
-let popupAboutMeInput = popupProfile.querySelector('.popup__about-me');
+let popupInputName = popupProfile.querySelector('.popup__user-name');
+let popupInputAbout = popupProfile.querySelector('.popup__about-me');
 
 
 // popup open
-buttonEdit.addEventListener('click', function() {
+function popupOpen(pop) {
+  // открываем переданный попап
+  pop.classList.add('popup_opened');
+  // отключаем скролл страницы
   body.classList.add('body_no-scroll');
-  popupProfile.classList.toggle('popup_opened');
 
-  popupNameInput.value = profileUserName.textContent;
-  popupAboutMeInput.value = profileUserAbout.textContent;
+  // если открыт попап ред.проф. передаем "имя" и "о себе" со страницы в значения инпут
+  if (pop === popupProfile) {
+    popupInputName.value = profileUserName.textContent;
+    popupInputAbout.value = profileUserAbout.textContent;
+  }
+};
+
+// открыть попап ред.проф.
+buttonEdit.addEventListener('click', function() {
+  popupOpen(popupProfile);
 });
 
 
 // popup close
 popupButtonClose.addEventListener('click', function() {
-  body.classList.remove('body_no-scroll');
-  popupProfile.classList.remove('popup_opened');
-});
-
-
-// popup btn submit
-popupButtonSubmit.addEventListener('click', function() {
-  let validationInputName = popupNameInput.value.length;
-
-  if (validationInputName > 0) {
+    // закрываем попап
+    popup.classList.remove('popup_opened');
+    // включаем скролл страницы
     body.classList.remove('body_no-scroll');
-    popupProfile.classList.remove('popup_opened');
-  }
 });
 
 
 // handler form
 function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Эта строчка отменяет стандартную отправку формы.
+  evt.preventDefault();
 
-  profileUserName.textContent = popupNameInput.value;
-  profileUserAbout.textContent = popupAboutMeInput.value;
+  // передаем внесенные изминения "имя" и "о себе" на страницу
+  profileUserName.textContent = popupInputName.value;
+  profileUserAbout.textContent = popupInputAbout.value;
+
+  // валидация инпут "имя" на пустое поле
+  let validationInputName = popupInputName.value.length;
+
+  if (validationInputName > 0) {
+    body.classList.remove('body_no-scroll');
+    popupProfile.classList.remove('popup_opened');
+  }
 }
 
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
