e454f5de35a9936d30a3d2fa40b984334e395354
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-02-02 00:24:57 +0300

    bugfix

--- script/index.js
+++ script/index.js
@@ -1,41 +1,40 @@
-const page = document.querySelector('.page');
-const buttonEdit = page.querySelector('.profile__edit-btn');
-const buttonAdd = page.querySelector('.profile__add-btn');
-const userName = page.querySelector('.profile__user-name');
-const userAbout = page.querySelector('.profile__user-about');
-const cardItems = page.querySelector('.card__items');
+const buttonEdit = document.querySelector('.profile__edit-btn');
+const buttonAdd = document.querySelector('.profile__add-btn');
+const userName = document.querySelector('.profile__user-name');
+const userAbout = document.querySelector('.profile__user-about');
+const cardItems = document.querySelector('.card__items');
 const cardTemplate = document.querySelector('.card-template').content;
-const popup = page.querySelectorAll('.popup');
-const popupProfile = page.querySelector('.popup_type_profile');
-const popupPhoto = page.querySelector('.popup_type_photo');
-const popupButtonClose = page.querySelectorAll('.popup__close');
-const popupInputName = page.querySelector('.popup__input_type_user-name');
-const popupInputAbout = page.querySelector('.popup__input_type_about-me');
+const popup = document.querySelectorAll('.popup');
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupPhoto = document.querySelector('.popup_type_photo');
+const popupButtonClose = document.querySelectorAll('.popup__close');
+const popupInputName = document.querySelector('.popup__input_type_user-name');
+const popupInputAbout = document.querySelector('.popup__input_type_about-me');
 
 const initialCards = [
   {
-		name: 'Звездное небо',
-		link: 'https://images.unsplash.com/photo-1643712662909-29fe8f02b613?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    name: 'Звездное небо',
+    link: 'https://images.unsplash.com/photo-1643712662909-29fe8f02b613?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   },
   {
-		name: 'Взлетаем',
-		link: 'https://images.unsplash.com/photo-1637477144793-cd3476659b0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80'
+    name: 'Взлетаем',
+    link: 'https://images.unsplash.com/photo-1637477144793-cd3476659b0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80'
   },
   {
-		name: 'Северное сияние',
-		link: 'https://images.unsplash.com/photo-1612686635542-2244ed9f8ddc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    name: 'Северное сияние',
+    link: 'https://images.unsplash.com/photo-1612686635542-2244ed9f8ddc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   },
   {
-		name: 'Закат',
-		link: 'https://images.unsplash.com/photo-1584377160571-1ea5df91fc75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=784&q=80'
+    name: 'Закат',
+    link: 'https://images.unsplash.com/photo-1584377160571-1ea5df91fc75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=784&q=80'
   },
   {
-		name: 'Лондонский мост',
-		link: 'https://images.unsplash.com/photo-1643574914412-409598704135?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    name: 'Лондонский мост',
+    link: 'https://images.unsplash.com/photo-1643574914412-409598704135?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   },
   {
-		name: 'Горячие источники',
-		link: 'https://images.unsplash.com/photo-1486108275492-35260a5d3318?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
+    name: 'Горячие источники',
+    link: 'https://images.unsplash.com/photo-1486108275492-35260a5d3318?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
   }
 ];
 
@@ -52,9 +51,9 @@ const cardRender = (name, link) => {
   cardLike.addEventListener('click', like);
   cardTrash.addEventListener('click', trash);
   cardImg.addEventListener('click', () => {
-    const popupFullscreen = page.querySelector('.popup_type_fullscreen');
-    const popupFullImg = page.querySelector('.popup__full-img');
-    const popupFullText = page.querySelector('.popup__full-text');
+    const popupFullscreen = document.querySelector('.popup_type_fullscreen');
+    const popupFullImg = document.querySelector('.popup__full-img');
+    const popupFullText = document.querySelector('.popup__full-text');
 
     popupFullImg.src = cardImg.src;
     popupFullText.textContent = cardText.textContent;
@@ -130,8 +129,8 @@ popupFormProfile.addEventListener('submit', formSubmitHandlerProfile);
 
 //handler form photo
 const popupFormPhoto = document.querySelector('.popup__form_type_photo');
-const popupInputCardName = page.querySelector('.popup__input_type_card-name');
-const popupInputCardLink = page.querySelector('.popup__input_type_card-link');
+const popupInputCardName = document.querySelector('.popup__input_type_card-name');
+const popupInputCardLink = document.querySelector('.popup__input_type_card-link');
 
 const formSubmitHandlerAddCard = evt => {
   evt.preventDefault();
